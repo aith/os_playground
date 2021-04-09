f13a6254da17f9f6c995ca460b6f48aebf087c46
aith <ariiramanesh@gmail.com>
2021-04-09 16:06:19 -0700

    added assignment1 starter

---
+++ assignment1_multiprocess_mergesort/psort.c
@@ -0,0 +1,40 @@
+/************************************************************************
+ * 
+ * CSE130 Assignment 1
+ *  
+ * POSIX Shared Memory Multi-Process Merge Sort
+ * 
+ * Copyright (C) 2020-2021 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without 
+ * the express written permission of the copyright holder.
+ *
+ ************************************************************************/
+
+#include "merge.h"
+
+/* 
+ * Merge Sort in the current process a sub-array of ARR[] defined by the 
+ * LEFT and RIGHT indexes.
+ */
+void singleProcessMergeSort(int arr[], int left, int right) 
+{
+  if (left < right) {
+    int middle = (left+right)/2;
+    singleProcessMergeSort(arr, left, middle); 
+    singleProcessMergeSort(arr, middle+1, right); 
+    merge(arr, left, middle, right); 
+  } 
+}
+
+/* 
+ * Merge Sort in the current process and at least one child process a 
+ * sub-array of ARR[] defined by the LEFT and RIGHT indexes.
+ */
+void multiProcessMergeSort(int arr[], int left, int right) 
+{
+  // Delete this line, it's only here to fail the code quality check
+  int i = 0;
+
+  // Your code goes here 
+}
