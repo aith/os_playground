5d05cc7efcadc2a9100307c0d7214f7c86783862
aith <ariiramanesh@gmail.com>
2021-04-14 14:53:19 -0700

    added single pthread

--- a2_multithreaded_mergesort/sort.c
+++ a2_multithreaded_mergesort/sort.c
@@ -1,23 +1,36 @@
 #include "merge.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
-void singleThreadedMergeSort(int arr[], int left, int right) 
-{
+void singleThreadedMergeSort(int arr[], int left, int right) {
   if (left < right) {
-    int middle = (left+right)/2;
-    singleThreadedMergeSort(arr, left, middle); 
-    singleThreadedMergeSort(arr, middle+1, right); 
-    merge(arr, left, middle, right); 
-  } 
+    int middle = (left + right) / 2;
+    singleThreadedMergeSort(arr, left, middle);
+    singleThreadedMergeSort(arr, middle + 1, right);
+    merge(arr, left, middle, right);
+  }
 }
 
-/* 
+/*
  * This function stub needs to be completed
  */
-void multiThreadedMergeSort(int arr[], int left, int right) 
-{
-  // Delete this line, it's only here to fail the code quality check
-  int i = 0;
 
-  // Your code goes here 
+void *threadStart(void *vargp) {
+  printf("hi, i just started!\n");
+  return NULL;
+}
+
+void multiThreadedMergeSort(int arr[], int left, int right) {
+  // Delete this line, it's only here to fail the code quality check
+  /* int i = 0; */
+
+  // Your code goes here
+  pthread_t t;
+  pthread_create(&t, NULL, threadStart, NULL);
+  pthread_join(t, NULL);
+  printf("After Thread\n");
+  exit(0);
 }
