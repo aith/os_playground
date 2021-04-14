b040b6a2f59940a3f7bc024342d20f7ff7187c7f
aith <ariiramanesh@gmail.com>
2021-04-14 16:03:27 -0700

    added multithread base

--- a2_multithreaded_mergesort/sort.c
+++ a2_multithreaded_mergesort/sort.c
@@ -20,25 +20,38 @@ void singleThreadedMergeSort(int arr[], int left, int right) {
  * This function stub needs to be completed
  */
 
-void *threadStart(void *argp) {
+struct mergeArgs {
   int tid;
-  tid = (int)argp;
-  printf("hi, i just started! I'm process %d\n", tid);
-  /* return NULL; */
+  int start_index;
+  int length;
+  int *arr;
+} mergeA;
+
+void *threadMerge(void *argp) {
+  /* tid = (int)argp[0]; */
+  struct mergeArgs *targs = argp;
+  printf("I'm process %d, I have %d args\n,", targs->tid, targs->length);
   pthread_exit(NULL);
 }
 
+// right is the final number
 void multiThreadedMergeSort(int arr[], int left, int right) {
-  // Delete this line, it's only here to fail the code quality check
-  /* int i = 0; */
-
-  // Your code goes here
+  int length = right - left + 1; // numbor of variables in arr
+  int amt_per_t = (int)length / 4;
+  int amt_extra = (int)length % 4;
+  struct mergeArgs args[NUM_THREADS];
   pthread_t threads[NUM_THREADS];
   for (int i = 0; i < NUM_THREADS; i++) {
-    pthread_t thread =
-        pthread_create(&threads[i], NULL, threadStart, (void *)i);
+    args[i].tid = i;
+    args[i].start_index = amt_per_t * i; // TODO this might be wrong
+    args[i].length = amt_per_t;
+    args[i].arr = arr;
+    if (i == 0) {
+      args[i].length += amt_extra;
+    }
+    pthread_t thread = pthread_create(&threads[i], NULL, threadMerge, &args[i]);
     if (thread) {
-      printf("Error: could not create pthread!");
+      printf("Error: could not create pthread!\n");
       exit(1);
     }
   }
