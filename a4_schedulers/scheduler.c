be964e72af5bd4900ac3d962bbf45b5236aa27e6
aith <ariiramanesh@gmail.com>
2021-05-10 15:40:45 -0700

    finished FCFS multithreaded ahhhhhh

--- a4_schedulers/scheduler.c
+++ a4_schedulers/scheduler.c
@@ -12,6 +12,7 @@
 #define MAX_THREADS 2048
 void *ready_q;
 void *running_q;
+void *io_q;
 int t_total = 0;
 thread_t *t_current;
 int t_current_id = 0;
@@ -24,10 +25,10 @@ int wait_start = 0;
 int wait_end = 0;
 int turnaround_start = 0;
 int turnaround_end = 0;
-int waiting_time_start[MAX_THREADS];
-int waiting_time_end[MAX_THREADS];
-int turnaround_time_start[MAX_THREADS];
-int turnaround_time_end[MAX_THREADS];
+/* int waiting_time_start[MAX_THREADS]; */
+/* int waiting_time_end[MAX_THREADS]; */
+/* int turnaround_time_start[MAX_THREADS]; */
+/* int turnaround_time_end[MAX_THREADS]; */
 
 typedef struct __stats_by_thread_t {
   thread_t *t;
@@ -35,6 +36,8 @@ typedef struct __stats_by_thread_t {
   unsigned int waiting_time_end;
   unsigned int turnaround_time_start;
   unsigned int turnaround_time_end;
+  unsigned int io_waiting_time_start;
+  unsigned int io_waiting_time_end;
   struct __stats_t
       *stats; // Address of the first of THREAD_COUNT stats_t 'objects'
 } stats_by_thread_t;
@@ -43,18 +46,22 @@ stats_by_thread_t *current;
 unsigned int waiting_time_sum = 0;
 unsigned int turnaround_time_sum = 0;
 
+void sys_read(thread_t *);
+
 stats_by_thread_t *init_stats_by_thread_t(thread_t *t) {
   /* printf("initing\n"); */
   stats_by_thread_t *stats_by_t = malloc(sizeof(stats_by_thread_t));
   stats_by_t->stats = malloc(sizeof(stats_t));
   stats_by_t->stats->tstats = malloc(sizeof(stats_t)); // testing
-  stats_by_t->stats->tid = t_count + 1;
+  stats_by_t->stats->tid = t->tid;
   /* stats_by_t->t = t; */
   stats_by_t->t = t; // malloc(sizeof(thread_t));
   stats_by_t->waiting_time_start = 0;
   stats_by_t->waiting_time_end = 0;
   stats_by_t->turnaround_time_start = 0;
   stats_by_t->turnaround_time_end = 0;
+  stats_by_t->io_waiting_time_start = 0;
+  stats_by_t->io_waiting_time_end = 0;
   t_count++;
   return stats_by_t;
 }
@@ -71,12 +78,7 @@ void scheduler(enum algorithm algorithm, unsigned int quantum) {
   ready_q = (void *)queue_create();
   running_q = (void *)queue_create();
   t_stats_q = (void *)queue_create();
-  for (int i = 0; i < MAX_THREADS; i++) {
-    waiting_time_start[i] = 0;
-    waiting_time_end[i] = 0;
-    turnaround_time_start[i] = 0;
-    turnaround_time_end[i] = 0;
-  }
+  io_q = (void *)queue_create();
 }
 
 /**
@@ -85,35 +87,24 @@ void scheduler(enum algorithm algorithm, unsigned int quantum) {
  * tick have been made.
  */
 void tick() {
-
-  /* printf("did exit\n"); */
-  /* stats_by_thread_t *next; */
   if (queue_size(ready_q) != 0) {
     if (queue_size(running_q) == 0) {
       stats_by_thread_t *next = (stats_by_thread_t *)queue_dequeue(ready_q);
-      next->waiting_time_end = sim_time();
+      next->waiting_time_end += sim_time();
       queue_enqueue(running_q, next);
       sim_dispatch(next->t);
     }
   }
 }
 
-static int same_t(void *a, void *b) {
-  /* return &(((stats_by_thread_t *)a)->t) == b; */
-  /* printf("tid is %d\n", ((stats_by_thread_t *)a)->tid); */
-  /* printf("%d\n", ((stats_by_thread_t *)a)->stats->tid); */
-  /* printf("%d\n", &(((stats_by_thread_t *)a)->t) == b); */
-  return ((stats_by_thread_t *)a)->t == (thread_t *)b;
-  /* ; */
-  /* return ((outer_t*)outer)->threadname == (thread_t*)inner; */
-}
+/* static int same_t(void *a, void *b) { */
+/*   return ((stats_by_thread_t *)a)->t == (thread_t *)b; */
+/* } */
 
 /**
  * Thread T is ready to be scheduled for the first time.
  */
 void sys_exec(thread_t *t) {
-  /* Put thread in ready queue if CPU is occupied */
-  /* printf("in sys_exec\n"); */
   stats_by_thread_t *next = init_stats_by_thread_t(t);
   next->waiting_time_start = sim_time();
   next->turnaround_time_start = sim_time();
@@ -125,12 +116,16 @@ void sys_exec(thread_t *t) {
  * Thread T has completed execution and should never again be scheduled.
  */
 void sys_exit(thread_t *t) {
-  /* printf("%d", queue_size(running_q)); */
-  /* printf("in sys_exit\n"); */
   stats_by_thread_t *fin = (stats_by_thread_t *)queue_dequeue(running_q);
   fin->turnaround_time_end = sim_time() + 1; /* specified by handout */
 
-  unsigned int waiting_time = fin->waiting_time_end - fin->waiting_time_start;
+  printf("thread %d spent %d waiting\n", fin->stats->tid,
+         fin->waiting_time_end - fin->waiting_time_start);
+  printf("thread %d spent %d io_waiting\n", fin->stats->tid,
+         fin->io_waiting_time_end - fin->io_waiting_time_start);
+  unsigned int waiting_time = fin->waiting_time_end - fin->waiting_time_start +
+                              fin->io_waiting_time_end -
+                              fin->io_waiting_time_start;
   unsigned int turnaround_time =
       fin->turnaround_time_end - fin->turnaround_time_start;
   fin->stats->waiting_time = waiting_time;
@@ -139,31 +134,44 @@ void sys_exit(thread_t *t) {
   turnaround_time_sum += turnaround_time;
 }
 
+void put_on_io_wait(thread_t *t) {
+  stats_by_thread_t *paused = (stats_by_thread_t *)queue_dequeue(running_q);
+  queue_enqueue(io_q, paused);
+  paused->io_waiting_time_start += sim_time() + 1;
+}
+
 /**
  * Thread T has requested a read operation and is now in an I/O wait queue.
  * When the read operation starts, io_starting(T) will be called, when the
  * read operation completes, io_complete(T) will be called.
  */
-void sys_read(thread_t *t) {}
+void sys_read(thread_t *t) { put_on_io_wait(t); }
 
 /**
  * Thread T has requested a write operation and is now in an I/O wait queue.
  * When the write operation starts, io_starting(T) will be called, when the
  * write operation completes, io_complete(T) will be called.
  */
-void sys_write(thread_t *t) {}
+void sys_write(thread_t *t) { put_on_io_wait(t); }
 
 /**
  * An I/O operation requested by T has completed; T is now ready to be
  * scheduled again.
  */
-void io_complete(thread_t *t) {}
+void io_complete(thread_t *t) {
+  stats_by_thread_t *unpaused = (stats_by_thread_t *)queue_dequeue(io_q);
+  queue_enqueue(ready_q, unpaused);
+  unpaused->waiting_time_start += sim_time();
+}
 
 /**
  * An I/O operation requested by T is starting; T will not be ready for
  * scheduling until the operation completes.
  */
-void io_starting(thread_t *t) {}
+void io_starting(thread_t *t) {
+  stats_by_thread_t *head = (stats_by_thread_t *)queue_head(io_q);
+  head->io_waiting_time_end += sim_time() - 1;
+}
 
 /**
  * Return dynamically allocated stats for the scheduler simulation, see
@@ -172,7 +180,8 @@ void io_starting(thread_t *t) {}
  */
 
 static int tid_lt(void *a, void *b) {
-  return ((stats_by_thread_t *)a)->tid < ((stats_by_thread_t *)b)->tid;
+  return ((stats_by_thread_t *)a)->stats->tid <
+         ((stats_by_thread_t *)b)->stats->tid;
 }
 
 stats_t *stats() {
@@ -190,7 +199,7 @@ stats_t *stats() {
   all_stats->tstats = malloc(sizeof(stats_t) * all_stats->thread_count);
 
   /* Fill in tstats */
-  queue_sort(t_stats_q, tid_lt);
+  /* queue_sort(t_stats_q, tid_lt); */
   for (int i = 0; i < t_count; i++) {
     stats_by_thread_t *sbt = queue_dequeue(t_stats_q);
     int tid = sbt->stats->tid;
